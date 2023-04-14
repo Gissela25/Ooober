d7837faa4d9c829aac5e5e0e2c012652826ffd28
Gissela Serrano <55642333+Gissela25@users.noreply.github.com>
2023-04-14 12:34:28 -0600

    SigIn with Google

--- OooberUser/app/src/main/java/com/ooober/user/activities/SignInActivity.kt
+++ OooberUser/app/src/main/java/com/ooober/user/activities/SignInActivity.kt
@@ -4,13 +4,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.ooober.user.R
 import com.ooober.user.databinding.ActivityHomeBinding
 import com.ooober.user.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignInBinding
-
+    private lateinit var client: GoogleSignInClient
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
@@ -23,6 +31,16 @@ class SignInActivity : AppCompatActivity() {
 
         binding.btnESignIn.setOnClickListener { goToLogin() }
         binding.GosignIn.setOnClickListener { goToRegister() }
+
+        val options = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        client = GoogleSignIn.getClient(this, options)
+        binding.btnSgoogle.setOnClickListener {
+            val intent = client.signInIntent
+            startActivityForResult(intent, 10001)
+        }
     }
 
     private fun goToLogin() {
@@ -33,6 +51,31 @@ class SignInActivity : AppCompatActivity() {
     private fun goToRegister() {
         val i = Intent(this, RegisterActivity::class.java)
         startActivity(i)
+    }
+
+    private fun SignGoogle() {
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode==10001){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            val account = task.getResult(ApiException::class.java)
+            val credential = GoogleAuthProvider.getCredential(account.idToken,null)
+            FirebaseAuth.getInstance().signInWithCredential(credential)
+                .addOnCompleteListener{task->
+                    if(task.isSuccessful){
+
+                        val i  = Intent(this,MapActivity::class.java)
+                        startActivity(i)
+
+                    }else{
+                        Toast.makeText(this,task.exception?.message, Toast.LENGTH_SHORT).show()
+                    }
+
+                }
+        }
     }
 
 }
