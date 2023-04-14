64901489c170341602a8b27466a27192b4c45918
Jony-07 <jony25lopezml@gmail.com>
2023-04-13 23:20:04 -0600

    Uploaded OooberDriver Project

---
+++ OooberDriver/app/build/generated/source/buildConfig/debug/com/ooober/driver/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.ooober.driver;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.ooober.driver";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
