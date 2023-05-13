326cf9e96bd4bcbd355d33c10e6762f0650c7150
horusxci <horus91@live.com>
2023-05-12 22:25:26 -0600

    Agregada funcion para verificar email del usuario a la actividad Profile, falta vincularla a un boton o accion.

--- OooberDriver/app/src/main/java/com/ooober/driver/activities/ProfileActivity.kt
+++ OooberDriver/app/src/main/java/com/ooober/driver/activities/ProfileActivity.kt
@@ -3,6 +3,9 @@ package com.ooober.driver.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.ooober.driver.R
 import com.ooober.driver.databinding.ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
@@ -13,6 +16,23 @@ class ProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    //Invocar para enviar correo de verificacion al usuario
+    private fun verifyUserEmail(){
+        val auth = FirebaseAuth.getInstance()
+        val user = auth.currentUser
+        val userEmail = user?.email
+
+        user?.sendEmailVerification()
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(applicationContext, "${resources.getString(R.string.txt_resetPassMailSent)} $userEmail", Toast.LENGTH_LONG).show()
+                }
+                else{
+                    Toast.makeText(applicationContext, "Error",Toast.LENGTH_LONG).show()
+                }
+            }
     }
 
 
